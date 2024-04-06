270639f12f8bbdf25e0c720500e088866b35a3de
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-06 17:03:02 +0530

    Create GolfGame.js

---
+++ src/components/GolfGame.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+
+const GolfGame = () => {
+  const [isball, setisBall] = useState(false);
+  const [ballPosition, setBallPosition] = useState({ top: 0, left: 0 });
+
+  const handleKeyDown = (event) => {
+    if (event.key === "ArrowRight" || event.keyCode === 39) {
+        setBallPosition((prevPosition) => {
+    if (prevPosition === null) return null; 
+        return { ...prevPosition, left: (prevPosition.left || 0) + 5 };
+      }); 
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+  }, []);
+
+  const handleStartGame = () => {
+    setisBall(true);
+  };
+
+  return (
+    <div>
+      {isball ? (
+        <div
+          className="ball"
+          style={{
+             position: "absolute",
+            top: ballPosition.top,
+            left: ballPosition.left,
+          }}
+        ></div>
+      ) : (
+        <button className="start" onClick={handleStartGame}>
+          Start
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default GolfGame;
